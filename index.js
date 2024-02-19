c6fc63aa3dbd5d407d70695c9d373b30ea9ca274
la-rika <laramisiaforlani@gmail.com>
2024-02-19 18:08:53 +0100

    setup homepage

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import path from 'path';
 import { fileURLToPath } from 'url'; //soluzione per sistemare __dirname is not defined
 import ejs from 'ejs';
 import pg from 'pg';
+import _ from 'lodash';
+
 
 const app = express();
 const port = 3000;
 
 const __filename = fileURLToPath(import.meta.url); //soluzione per sistemare __dirname is not defined
 const __dirname = path.dirname(__filename); //soluzione per sistemare __dirname is not defined
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
     res.render('index.ejs')
