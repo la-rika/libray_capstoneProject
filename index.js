f473662bcbb699e33ef1fdbc2221718c6b4283e5
la-rika <laramisiaforlani@gmail.com>
2024-02-25 00:28:05 +0100

    data in from db in the card but need fixes

--- index.js
+++ index.js
@@ -8,14 +8,46 @@ import _ from 'lodash';
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "personal_library",
+    password: "M1server370!!",
+    port: 5432,
+})
+
+db.connect()
+
 const __filename = fileURLToPath(import.meta.url); //soluzione per sistemare __dirname is not defined
 const __dirname = path.dirname(__filename); //soluzione per sistemare __dirname is not defined
 app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.render('index.ejs')
+let books;
+
+app.get('/', async(req, res) => {
+    books = [];
+    try {
+        await db.query('SELECT * FROM books')
+        .then((response)=>{
+            // console.log(response.rows);
+            response.rows.forEach(el => {
+                books = [...books,{
+                    id: el.id,
+                    title: el.title,
+                    author: el.author,
+                    isbn: el.isbn,
+                    opinion_id: el.opinion_id
+                }]
+            });
+            // console.log(books)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+    res.render('index.ejs', {books:books})
 })
 
-app.listen(port||4000, ()=>{
+app.listen(port || 4000, () => {
     console.log(`server is up and running on port ${port}`)
 })
